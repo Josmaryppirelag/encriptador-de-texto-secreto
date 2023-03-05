17fffedd8513cea1c1b138e0f365e205b7e3f949
Josmary Pirela <marsonriente2000@gmail.com>
2023-03-04 21:00:15 -0400

    Avance en el archivo js

--- script.js
+++ script.js
@@ -1,3 +1,33 @@
-let entry = document.getElementById("textarea");
-let encryptButton = ;
-let decryptButton = ;
+function encrypt(){
+    let text= document.getElementById("textarea").value();
+    let textEncrypt= text.replace(/a/gm,"ai");
+    let textEncrypt= textEncrypt.replace(/e/gm,"enter");
+    let textEncrypt= textEncrypt.replace(/i/gm,"imes");
+    let textEncrypt= textEncrypt.replace(/o/gm,"ober");
+    let textEncrypt= textEncrypt.replace(/u/gm,"ufat");
+
+    document.getElementById("message").innerHTML= textEncrypt;
+    document.getElementById("copy").style.display= "Show";
+    document.getElementById("copy").style.display= "inherit";
+
+}
+
+function decrypt(){
+    let text= document.getElementById("textarea").value();
+    let textEncrypt= text.replace(/ai/gm,"a");
+    let textEncrypt= textEncrypt.replace(/enter/gm,"e");
+    let textEncrypt= textEncrypt.replace(/imes/gm,"i");
+    let textEncrypt= textEncrypt.replace(/ober/gm,"o");
+    let textEncrypt= textEncrypt.replace(/ufat/gm,"u");
+
+    document.getElementById("message").innerHTML= textEncrypt;
+    document.getElementById("copy").style.display= "Show";
+    document.getElementById("copy").style.display= "inherit";
+
+}
+
+function copiar(){
+    let content= document.querySelector("message");
+    content.select();
+    document.execCommand("copy");
+}
